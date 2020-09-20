498c1a1ec32d646cac8fa08c927931aa49d0b699
Kianoosh Sanatkar <48183332+kianooshsanatkar@users.noreply.github.com>
2020-09-21 00:16:07 +0330

    Create Movie Repository

---
+++ Repository/MovieContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using MovieCollection.Domain.Entities;
+
+namespace MovieCollection.DataAccess.Repository
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext()
+        {
+
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; }
+        public virtual DbSet<Person> People { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+        public virtual DbSet<Genres> Genres { get; set; }
+
+        
+    }
+}
---
+++ Repository/MovieUnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using MovieCollection.DataAccess.BaseRepository;
+using MovieCollection.DataAccess.IBaseRepository;
+using MovieCollection.Domain.Entities;
+using System;
+
+namespace MovieCollection.DataAccess.Repository
+{
+    public class MovieUnitOfWork : IUnitOfWork
+    {
+        private readonly MovieContext _context;
+        public IRepository<Movie> MovieRepo { get; private set; }
+        public IRepository<Person> PersonRepo { get; private set; }
+        public IRepository<Tag> TagRepo { get; private set; }
+        public IRepository<Genres> GenreRepo { get; private set; }
+
+        public MovieUnitOfWork(MovieContext context)
+        {
+            _context = context;
+            MovieRepo = new Repository<Movie>(_context);
+            PersonRepo = new Repository<Person>(_context);
+            TagRepo = new Repository<Tag>(_context);
+            GenreRepo = new Repository<Genres>(_context);
+        }
+
+        public int Complete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
