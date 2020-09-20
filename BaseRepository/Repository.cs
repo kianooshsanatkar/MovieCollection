4f305a07f10c6f861d5cda81bf144febefe50e76
Kianoosh Sanatkar <48183332+kianooshsanatkar@users.noreply.github.com>
2020-09-21 00:08:35 +0330

    Create: Generic Repository Implementation

---
+++ BaseRepository/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.Entity;
+using MovieCollection.DataAccess.IBaseRepository;
+using MovieCollection.Domain.CoreDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MovieCollection.DataAccess.BaseRepository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        protected readonly DbContext context;
+        public Repository(DbContext context)
+        {
+            this.context = context;
+        }
+        public void Add(TEntity entity)
+        {
+            context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            context.Set<TEntity>().AddRange(entities);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return context.Set<TEntity>().Where(predicate).ToList();
+        }
+
+        public TEntity FindSingleOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return context.Set<TEntity>().Where(predicate).FirstOrDefault();
+        }
+
+        public TEntity Get(Guid id)
+        {
+            return context.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            context.Set<TEntity>().RemoveRange(entities);
+        }
+    }
+}
